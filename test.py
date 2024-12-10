865a8783e70b58aebf3134a232bc9d6fbea1a995
Harshal12355 <harshalts@gmail.com>
2024-12-10 23:19:49 +0400

    Cleaned up more

--- test.py
+++
@@ -1,8 +0,0 @@
-from chatbot import chatbot_instance
-from models.instances.mistral import mistral_chat_instance
-
-print('Created Chatbot Instance')
-
-# Query the chatbot
-response = mistral_chat_instance.query("What is the purpose of life?")
-print("Response: ", response["messages"][-1]["content"])
